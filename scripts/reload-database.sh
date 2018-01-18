4ad17f807de7e64c11a97db9326171d9905db601
thobalose <lose.thoba@gmail.com>
2018-01-18 13:47:33 +0200

    reload db cron

---
+++ scripts/reload-database.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Checks if the gff2neo container exists and has exited. It the restarts it else
+# Runs container from image
+
+CONT_NAME="gff2neo_gff2neo_1"
+IMAGE_NAME="gff2neo_gff2neo"
+CID="$(docker ps -aq -f status=exited -f name=${CONT_NAME})"
+
+if [ CID ]; then
+ echo "Restarting ${CONT_NAME} with ID: ${CID}..."
+ docker restart ${CONT_NAME}
+else
+ echo "Running ${CONT_NAME} from ${IMAGE_NAME}..."
+ docker run -d --name ${CONT_NAME} ${IMAGE_NAME}
+fi
+
+
